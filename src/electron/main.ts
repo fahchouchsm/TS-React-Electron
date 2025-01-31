e8b96590513ee5ce31e9c2630e2087e2a6803d72
Fahchouch Mohammed <fahchouchsm@gmail.com>
2025-01-31 19:43:46 +0100

    Electron typescript react boilet plate is ready

---
+++ src/electron/main.ts
@@ -0,0 +1,17 @@
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  mainWindow.loadFile(path.join(app.getAppPath(), '/distReact/index.html'));
+}
+
+app.on('ready', createWindow);
